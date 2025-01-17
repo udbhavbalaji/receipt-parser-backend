4c4cc7d43228df9384fb373520158b769e6b4f22
udbhavbalaji <udbhavbalaji@gmail.com>
2025-01-17 14:53:38 -0500

    Added logic in error-handling middleware to bypass route logic whenever logout is required.

--- src/handlers/error.ts
+++ src/handlers/error.ts
@@ -5,35 +5,46 @@ import { HTTPStatusCodes } from "src/types/enums";
 
 const handle = (method: Function) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(method(req, res, next))
-      .then(
-        ({
-          status,
-          responseBody,
-        }: {
-          status: HTTPStatusCodes;
-          responseBody?: Record<string, any>;
-        }) => {
-          if (responseBody) {
-            res.status(status).json(responseBody);
-          } else {
-            res.sendStatus(status);
-          }
-        }
-      )
-      .catch((err) => {
-        let exception: SpentAPIException;
-
-        if (err instanceof SpentAPIException) {
-          exception = err;
-        } else {
-          exception = throwInternalServerError(
-            "Something went wrong :(",
-            err.stack
-          );
-        }
-        next(exception);
+    if (
+      req.headers.logout_required === "Y" &&
+      req.headers.authorization === "redacted"
+    ) {
+      res.status(HTTPStatusCodes.REDIRECT_REQUIRED).json({
+        message: "User must be logged out",
+        redirectTo: "/api/auth/logout",
+        method: "PUT",
       });
+    } else {
+      Promise.resolve(method(req, res, next))
+        .then(
+          ({
+            status,
+            responseBody,
+          }: {
+            status: HTTPStatusCodes;
+            responseBody?: Record<string, any>;
+          }) => {
+            if (responseBody) {
+              res.status(status).json(responseBody);
+            } else {
+              res.sendStatus(status);
+            }
+          }
+        )
+        .catch((err) => {
+          let exception: SpentAPIException;
+
+          if (err instanceof SpentAPIException) {
+            exception = err;
+          } else {
+            exception = throwInternalServerError(
+              "Something went wrong :(",
+              err.stack
+            );
+          }
+          next(exception);
+        });
+    }
   };
 };
 
