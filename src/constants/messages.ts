0e7638431073a0fa4bf2d8856cd71894b872e48b
udbhavbalaji <udbhavbalaji@gmail.com>
2025-01-16 20:34:46 -0500

    built verifier system that ensures that only clients in possession of secret app key can request the server

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -2,5 +2,13 @@ const InvalidEmailMessage = "Invalid email. Enter a valid email to register.";
 
 const PasswordTooShortMessage =
   "Your password must be at least 8 characters long.";
+const MissingSecretAppKeyErrorMessage =
+  "Your app is not verified. Contact the devs to get access.";
+const InvalidSecretAppKeyErrorMessage = "Invalid app key";
 
-export default { InvalidEmailMessage, PasswordTooShortMessage };
+export default {
+  InvalidEmailMessage,
+  PasswordTooShortMessage,
+  MissingSecretAppKeyErrorMessage,
+  InvalidSecretAppKeyErrorMessage,
+};
--- src/constants/secrets.ts
+++ src/constants/secrets.ts
@@ -3,5 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.APP_PORT || 3000;
+const SECRET_APP_KEY = process.env.SECRET_APP_KEY || "testing123";
 
-export default { PORT };
+export default { PORT, SECRET_APP_KEY };
